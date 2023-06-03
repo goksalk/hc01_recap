1ca5590434e39b1d97e35f258034d2897817f1c9
goksalk <goksalkarabulut93@gmail.com>
2023-06-03 12:21:34 +0300

    yenilikler

---
+++ DOM2/event.js
@@ -0,0 +1,8 @@
+// const h1 = document.querySelector("header h1").onmouseover = Function() {
+//    document.querySelector("header h1").style.color="red"
+// }
+
+const heading1 = document.querySelector("header h1");
+heading1 = onmuseover = function () {
+  heading1.style.color = "red";
+};
--- DOM2/index.js
+++ DOM2/index.js
@@ -10,3 +10,29 @@ const text = document.createTextNode(langInput.value);
 newP.appendChild(text);
 
 console.log(newP);
+
+const body = document.querySelector("main");
+body.appendChild(newP);
+
+const itemListSection = document.querySelector(".item-list");
+itemListSection.before(newP);
+
+newP.id = "paragraf";
+newP.className = "par center";
+
+// !setAtrrribute ile id clas değitirme
+
+newP.setAttribute("id", "new-id");
+newP.setAttribute("class", "new-class");
+
+const newDıv = document.createElement("div");
+newDıv.id = "my-div";
+
+itemListSection.after(newDıv);
+newDıv.innerHTML += `
+<h2 clas="par center">languages</h2>
+<ul>
+<li>C++</li>
+<li>java</li>
+<li>go</li>
+</ul>`;
